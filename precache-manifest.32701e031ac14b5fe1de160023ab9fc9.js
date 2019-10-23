f04234eb8e1fdbfbdaa5df8d38464b3f8b14fac8
ENTIN WEI <galadiya41@gmail.com>
2019-10-23 12:50:06 +0800

    Updates

---
+++ precache-manifest.32701e031ac14b5fe1de160023ab9fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a625dfab12124c357f1fea3d4d89b3",
+    "url": "/github-repo-list/index.html"
+  },
+  {
+    "revision": "8b412cb0e02d00aa74d3",
+    "url": "/github-repo-list/static/css/main.08d48b81.chunk.css"
+  },
+  {
+    "revision": "2d8b0f1ccad056cf3fdb",
+    "url": "/github-repo-list/static/js/2.4eeb4682.chunk.js"
+  },
+  {
+    "revision": "8b412cb0e02d00aa74d3",
+    "url": "/github-repo-list/static/js/main.763a453f.chunk.js"
+  },
+  {
+    "revision": "4977dbc10845d36fb2ad",
+    "url": "/github-repo-list/static/js/runtime-main.d68f2965.js"
+  }
+]);
